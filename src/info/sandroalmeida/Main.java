592f7ae9001dd3f90615fea0e169594d513287b2
sandroalmeida <sandro.almeida@dotbuild.com.br>
2019-09-19 08:13:44 -0700

    First commit

---
+++ src/info/sandroalmeida/Main.java
@@ -0,0 +1,13 @@
+package info.sandroalmeida;
+
+import info.sandroalmeida.easy.TwoSum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TwoSum.solution();
+
+
+    }
+}
---
+++ src/info/sandroalmeida/easy/TwoSum.java
@@ -0,0 +1,44 @@
+package info.sandroalmeida.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    /*
+    *   Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        Example:
+        Given nums = [2, 7, 11, 15], target = 9,
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+    * */
+
+    public static void solution(){
+
+        // initial parameters
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        // solution code
+        Map<Integer, Integer> rests = new HashMap<>();
+        int[] result = {-1, -1};
+
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            int rest = target - num;
+            if(rests.containsKey(num)){
+                result[0] = rests.get(num);
+                result[1] = i;
+            } else{
+                rests.put(rest, i);
+            }
+        }
+
+        System.out.println(rests);
+        System.out.println("(" + result[0] + "),(" + result[1] + ")");
+
+    }
+
+
+}
