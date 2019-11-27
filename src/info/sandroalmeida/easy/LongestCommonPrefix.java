6cbfbee0abc8e81b78b6522431fe754ad4e640cb
sandroalmeida <sandro.almeida@dotbuild.com.br>
2019-11-27 10:17:29 -0800

    Leetcode 14 - Longest Common Prefix (Brute Force Solution).

--- src/info/sandroalmeida/easy/LongestCommonPrefix.java
+++ src/info/sandroalmeida/easy/LongestCommonPrefix.java
@@ -22,48 +22,27 @@ public class LongestCommonPrefix {
     * */
     public static String solution(String[] strs){
 
-        char[] first = null;
-        char[] second = null;
-        char[] longest;
-
-        if(strs != null)
-            first = strs[0].toCharArray();
-        else
-            return "";
-
-        if(strs.length >= 1)
-            second = strs[1].toCharArray();
-        else
-            return  "";
-
-        String longestStr = compare(first, second);
-        longest = longestStr.toCharArray();
-
-        if(strs.length >= 2){
-            for(int i = 2; i < strs.length; i++){
-                String newStr = compare(longest, strs[i].toCharArray());
-                if(newStr != null){
-                    longestStr = newStr;
-                    longest = longestStr.toCharArray();
-                    continue;
+        String result = "";
+        if(strs != null){
+            String first = strs[0];
+            boolean check = false;
+            for(int i = 0; i < first.length(); i++){
+                for(int z = i+1; z <= first.length(); z++){
+                    String part = first.substring(i,z);
+                    for(int x = 1; x < strs.length; x++){
+                        if(strs[x].contains(part))
+                            check = true;
+                        else{
+                            check = false;
+                            break;
+                        }
+                    }
+                    if(part.length() > result.length() && check)
+                        result = part;
                 }
             }
         }
-        return longestStr;
+        return result;
     }
-
-    private static String compare(char[] s1, char[] s2){
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < s1.length; i++){
-            for(int x = 0; x < s2.length; x++){
-                if(s1[i] == s2[x]){
-                    result.append(s1[i]);
-                    break;
-                }
-            }
-        }
-        return (result.toString());
-    }
-
 
 }
